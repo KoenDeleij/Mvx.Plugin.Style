2e7a1c14ddd5a34b1173201108c9c7d31761026c
Koen Deleij <koen.deleij@redhotminute.com>
2016-12-22 14:09:17 +0100

    Use the loader plugin bootstrap. Default the plugin will search in the pcl library.

--- Bootstrap/BootstrapDroid/RHMStylePluginBootstrap.cs.pp
+++ Bootstrap/BootstrapDroid/RHMStylePluginBootstrap.cs.pp
@@ -3,7 +3,7 @@ using MvvmCross.Platform.Plugins;
 
 namespace Redhotminute.Mvx.Plugin.Style.Droid
 {
-	public class RHMStylePluginBootstrap : MvxPluginBootstrapAction<Redhotminute.Mvx.Plugin.Style.Droid.PluginLoader>
+	public class RHMStylePluginBootstrap : MvxLoaderPluginBootstrapAction<Redhotminute.Mvx.Plugin.Style.Droid.PluginLoader,Redhotminute.Mvx.Plugin.Style.Droid.Plugin>
 	{
 	}
 }
