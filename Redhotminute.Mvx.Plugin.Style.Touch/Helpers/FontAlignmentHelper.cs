7a29bb1076dc50104d0446ab7deb302066505670
Koen Deleij <koen.deleij@redhotminute.com>
2017-01-27 14:59:02 +0100

    remove breaks

--- Redhotminute.Mvx.Plugin.Style.Touch/Helpers/FontAlignmentHelper.cs
+++ Redhotminute.Mvx.Plugin.Style.Touch/Helpers/FontAlignmentHelper.cs
@@ -6,10 +6,10 @@ namespace Redhotminute.Mvx.Plugin.Style.Touch {
 		public static UITextAlignment ToNativeAlignment(this Font font) {
 
 			switch (font.Alignment) {
-				case TextAlignment.Left: return UITextAlignment.Left;break;
-				case TextAlignment.Center: return UITextAlignment.Center; break;
-				case TextAlignment.Right: return UITextAlignment.Right; break;
-				case TextAlignment.Justified: return UITextAlignment.Justified; break;
+				case TextAlignment.Left: return UITextAlignment.Left;
+				case TextAlignment.Center: return UITextAlignment.Center; 
+				case TextAlignment.Right: return UITextAlignment.Right;
+				case TextAlignment.Justified: return UITextAlignment.Justified; 
 			}
 			return UITextAlignment.Left;
 		}
