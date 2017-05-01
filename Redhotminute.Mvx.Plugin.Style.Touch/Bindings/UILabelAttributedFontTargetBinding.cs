2cbe3221338ed48df7870a73e92fff7fcc065754
Koen Deleij <koen.deleij@redhotminute.com>
2017-05-01 17:10:57 +0200

    Add a fix to set the text alignment behaviour for iOS

--- Redhotminute.Mvx.Plugin.Style.Touch/Bindings/UILabelAttributedFontTargetBinding.cs
+++ Redhotminute.Mvx.Plugin.Style.Touch/Bindings/UILabelAttributedFontTargetBinding.cs
@@ -21,10 +21,27 @@ namespace Redhotminute.Mvx.Plugin.Style.Touch
 			var label = (UILabel)target;
 			if (value != null) {
 				try {
-					label.AttributedText = value as NSAttributedString;
+					var text = value as NSAttributedString;
+					//override the textalignment in case the view is not natural so that we can use the designer to deside the alignment (what you want in most cases)
+					if (label.TextAlignment != UITextAlignment.Natural) {
+						text.EnumerateAttributes(new NSRange(0, text.Length - 1), NSAttributedStringEnumeration.None, new NSAttributedRangeCallback(ResetAlignment));
+					}
+					label.AttributedText = text;
 				}
 				catch (Exception e) {
 					MvvmCross.Platform.Mvx.Trace(MvvmCross.Platform.Platform.MvxTraceLevel.Error, "Failed to set font+language to UILabel. Check if font exists, has a size and filename, and is added to the plist");
+				}
+			}
+		}
+
+		private void ResetAlignment(NSDictionary dic, NSRange range, ref bool stop) {
+			if (dic.ContainsKey(new NSString("NSParagraphStyle"))) {
+				//set the alignment to the alignment of the label
+				NSObject val;
+				dic.TryGetValue(new NSString("NSParagraphStyle"), out val);
+				NSParagraphStyle style = val as NSParagraphStyle;
+				if (style != null) {
+					style.Alignment = (this.Target as UILabel).TextAlignment;
 				}
 			}
 		}
