3a4fde0fbe91299301f64ad05d4c86f4e5a68d22
Koen Deleij <koen.deleij@redhotminute.com>
2018-04-19 21:47:14 +0200

    Add alignment check for UITextview (use the textview's allignment if not natural)

--- Redhotminute.Mvx.Plugin.Style.Touch/Bindings/UITextViewAttributedFontTargetBinding.cs
+++ Redhotminute.Mvx.Plugin.Style.Touch/Bindings/UITextViewAttributedFontTargetBinding.cs
@@ -13,16 +13,21 @@ namespace Redhotminute.Mvx.Plugin.Style.Touch.Bindings
 
         protected override void SetValueImpl(object target, object value)
         {
-            var label = (UITextView)target;
+            var textView = (UITextView)target;
             if (value != null)
             {
                 try
                 {
                     var text = value as NSAttributedString;
 
+                    if (textView.TextAlignment != UITextAlignment.Natural && text.Length > 0)
+                    {
+                        text.EnumerateAttributes(new NSRange(0, text.Length - 1), NSAttributedStringEnumeration.None, new NSAttributedRangeCallback(ResetAlignment));
+                    }
+
                     text.EnumerateAttributes(new NSRange(0, text.Length - 1), NSAttributedStringEnumeration.None, new NSAttributedRangeCallback(FindLink));
                 
-                    label.AttributedText = text;
+                    textView.AttributedText = text;
                 }
                 catch (Exception e)
                 {
@@ -40,6 +45,21 @@ namespace Redhotminute.Mvx.Plugin.Style.Touch.Bindings
                 UIColor color = val as UIColor;
                 if(color!= null){
                     (Target as UITextView).TintColor = color;
+                }
+            }
+        }
+
+        private void ResetAlignment(NSDictionary dic, NSRange range, ref bool stop)
+        {
+            if (dic.ContainsKey(new NSString("NSParagraphStyle")))
+            {
+                //set the alignment to the alignment of the label
+                NSObject val;
+                dic.TryGetValue(new NSString("NSParagraphStyle"), out val);
+                NSParagraphStyle style = val as NSParagraphStyle;
+                if (style != null)
+                {
+                    style.Alignment = (this.Target as UITextView).TextAlignment;
                 }
             }
         }
